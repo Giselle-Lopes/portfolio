e66a2d408172785c61d4eb0ffd6ad0b78e20bd08
Giselle Lopes <giselle.lopes@poatek.com>
2024-12-16 11:07:55 -0300

    dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      concertOne: ["Concert One", "serif"],
+      mono: ["Geist Mono"]
+    },
+    extend: {
+      animation: {
+        'displayText': 'displayText 3s linear infinite',
+        'animate': 'animate 3s linear infinite'
+      },
+      keyframes: {
+        displayText: {
+          '0%': {
+            opacity: '1',
+            visibility: 'visible',
+          },
+          '33.33%, 100%': {
+            opacity: '0',
+            visibility: 'hidden',
+          },
+        },
+      }
+    },
   },
   plugins: [],
 }
