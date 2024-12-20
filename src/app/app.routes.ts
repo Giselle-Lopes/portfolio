bc2a3d0005dcd0564bdfa424240e2c24d73ba5a0
Giselle Lopes <giselle.lopes@poatek.com>
2024-12-20 09:03:13 -0300

    fix route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ChallengesComponent } from './pages/challenges/challenges.component';
 import { CalculatorComponent } from './pages/calculator/calculator.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { CalenderComponent } from './calender/calender.component';
+import { CalenderComponent } from './pages/calender/calender.component';
 import { CollectionsComponent } from './pages/collections/collections.component';
 
 export const routes: Routes = [
